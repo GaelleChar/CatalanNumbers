b9ae03dab1fc9187bb1b643f20556323d7140a6f
Gaelle <milytulip9@gmail.com>
2022-02-27 20:10:10 -0500

    Add files via upload

--- Catalan.js
+++ Catalan.js
@@ -2,73 +2,81 @@ let $ = selector => document.querySelector(selector);
 let $$ = selector => document.querySelectorAll(selector);
 
 let cat = null;
-
 class Cat{ 
     /*
     Equation for Catalan Number:
     (2n)!/((n+1)!n!)
     */
-    constructor(){
-        this.x = null;
-        this.num = null;
+    constructor(num){
+        this.num = num;
     }
 	
 }
 function main(){
+}
     
     //to allow txt to change to Catalan
     const btn = $(`button`);
-    const txt = $(`p`);
+    const txt = document.getElementById(`hidden`);
 
     btn.addEventListener("click", (factorial) =>{
 
-        //let user input a num
-        //TODO: should be in 'DOMContentLoaded' event
-        var num = document.getElementById(`input`).value;
+        const num = document.getElementById(`input`).value;
 
-        function factorial(num){
-            var res = 0;
-            //Base case
-            if (num<=1){
-                return;
+        function factorial(num) {
+            let maxStack = num - 1;
+            var x = num;
+
+            if (maxStack > 0) {
+                return x * factorial(maxStack);  
             }
-            else{
-                for (var i = 0; i < num; i++){
-                    res += factorial(i) * factorial(num-i-1)
-                }
-            }
-            return res;
-        }
+            
+            return x;
+        }    
         function CatalanTop(num){
             var top = 2 * num;
             return factorial(top)
         }
         function CatalanBottom(num){
-            var first = factorial(num + 1);
+       			const one = parseInt(num) + 1;
+            var first = factorial(one);
             var second = factorial(num)
             return first * second;
         }
 
+        factorial(num);
+        
+        CatalanTop(num);
+        
+        CatalanBottom(num);
+
         const top = CatalanTop(num);
         const bottom = CatalanBottom(num);
         const Catalan = top/bottom;
+
+        console.log("Num inserted: " + num)
+        console.log("Catalan Num = " + Catalan)
         
         //print out Catalan Number where "calculate" button is
-        if (btn.textContent === 'Calculate') {
+        if (btn.type === 'submit') {
             txt.textContent = 'Answer: ' + Catalan;
         }
         else{
-            //txt.textContent = 'Enter Another Number: ';
+            //txt.textContent = 'Enter Another Number: '
+            console.log("button or paragraph text not working");
         }
     });
     
     //document.getElementById("answer").innerHTML = main();
-}
 function AttachEventsFor(parentElement) {
 }
-
-window.addEventListener('DOMContentLoaded', event => {
-    cat = new Cat();
-    //var x = document.getElementById(`input`).value;
-    //let num = x;
-})
+if(typeof window !== undefined){
+    window.addEventListener('DOMContentLoaded', (event) => {
+        cat = new Cat();
+        cat.num = document.getElementById(`input`).value;
+        console.log('DOM fully loaded and parsed');
+    })
+}
+else{
+    console.log('DOM not loaded');
+}
